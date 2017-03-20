92f9561ce74707f41401bdf3a943eb9200559792
TarikHuber <huber.tarik@gmail.com>
2017-03-20 08:34:05 +0100

    Refactored to use react ref callback

--- src/ResponsiveMenu.js
+++ src/ResponsiveMenu.js
@@ -6,23 +6,19 @@ import Measure from 'react-measure';
 
 const styles={
   menu_container:{
-    //display: 'inline-block',
     display: 'flex',
     width: '100%',
     justifyContent:'flex-end',
     margin: 0,
     padding: 0,
-    //overflow: 'hidden'
   }
 }
 
 class ResponsiveMenu extends Component {
 
   state = {
-    initList: [],
     iconList: [],
-    menuList: [],
-    dimensions: {}
+    menuList: []
   };
 
   arraysContainSame = (a, b) => {
@@ -33,26 +29,17 @@ class ResponsiveMenu extends Component {
 
   componentDidMount(){
     const { menuList} = this.props;
-    this.setState({initList: menuList});
-    const dimensions ={width: this.refs.menuContainer.offsetWidth};
-    this.setState({dimensions});
     this.updateLists(this.props);
-
-    //var width = this.refs.menuContainer.offsetWidth;
-    //console.log(dimensions);
   }
 
   handleWidthChange = (dimensions) => {
-    this.setState({dimensions});
     this.updateLists(this.props);
   }
 
   componentWillReceiveProps(nextProps){
-
     if(nextProps!==undefined && !this.arraysContainSame(this.props.menuList, nextProps.menuList)){
       this.updateLists(nextProps);
     }
-
   }
 
   updateLists = (props) => {
@@ -60,20 +47,24 @@ class ResponsiveMenu extends Component {
     const { menuList, iconWidth, marginCorrection } = props;
 
     const itemsCount=menuList.filter((item)=>!item.hidden).length;
-    const dimensions=this.state.dimensions;
+    const width =this.menuContainer.offsetWidth;
     const iconW=iconWidth!==undefined?iconWidth:48;
     const correction=marginCorrection!==undefined?marginCorrection:0;
     const breackLimit=itemsCount*iconW;
 
-    const hiddenWidth=breackLimit-dimensions.width-correction;
+    const hiddenWidth=breackLimit-width-correction;
     const hiddenIcons=Math.ceil(hiddenWidth/iconW)+1;
 
     if(hiddenIcons > 0){
-      this.setState({menuList: menuList.slice(menuList.length-hiddenIcons, menuList.length)})
-      this.setState({iconList: menuList.slice(0, hiddenIcons*-1)})
+      this.setState({
+        menuList: menuList.slice(menuList.length-hiddenIcons, menuList.length),
+        iconList: menuList.slice(0, hiddenIcons*-1)
+      });
     }else{
-      this.setState({menuList: []})
-      this.setState({iconList: menuList})
+      this.setState({
+        menuList: [],
+        iconList: menuList
+      });
     }
   }
 
@@ -107,7 +98,11 @@ class ResponsiveMenu extends Component {
         whitelist={['width']}
         blacklist={['height', 'top', 'right', 'bottom', 'left']}>
 
-        <div style={styles.menu_container} ref="menuContainer">
+        <div
+          style={styles.menu_container}
+          ref={(container)=>{
+            this.menuContainer=container;
+          }}>
 
           {iconList.filter((item)=>!item.hidden).map(item =>this.getIcon(item))}
 
